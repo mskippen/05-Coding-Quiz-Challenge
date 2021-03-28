48da05a7a3995fa5217af80877ac05fe89fcdab6
Megan Skippen <79444992+mskippen@users.noreply.github.com>
2021-03-29 00:26:03 +1100

    html and css and draft questions in .js setup

---
+++ script.js
@@ -0,0 +1,50 @@
+var questions = [
+    {
+      numb: 1,
+      question: "Which of the following is not an array method?",
+      answer: "toLowercase",
+      options: ["toLowercase", "forEach", "map", "for loop"],
+    },
+    {
+      numb: 2,
+      question: 'How do you call a function named "myFunction"',
+      answer: "myFunction()",
+      options: [
+        "myFunction{}",
+        "call function myFunction()",
+        "call myFunction()",
+        "myFunction()",
+      ],
+    },
+    {
+      numb: 3,
+      question: 'How do you write "Hello World" in an alert box?',
+      answer: 'alert("Hello World")',
+      options: [
+        'alert("Hello World")',
+        'msg("Hello World")',
+        'alertBox("Hello World")',
+        "All of the above",
+      ],
+    },
+    {
+      numb: 4,
+      question:
+        "Which of the following is not a data type supported by Javascript?",
+      answer: "echo",
+      options: ["Undefined", "Null", "Boolean", "echo"],
+    },
+    {
+      numb: 5,
+      question: "How do you create a function in JavaScript?",
+      answer: "function = myFunction()",
+      options: [
+        "function myFunction()",
+        "function = myFunction()",
+        "function:myFunction()",
+        "function == myFunction()",
+      ],
+    },
+];
+
+console.log(questions)
